85aab99fa095974865847e2c78601bdd99c2fd14
yidaoxiangan <test@test.com>
2021-05-17 21:30:18 +0800

    modify return value of download module

--- url_parse/download_module.py
+++ url_parse/download_module.py
@@ -1,26 +1,25 @@
 from django.http import FileResponse
 from django.http import HttpResponseNotAllowed
 from django.http import HttpResponseNotFound
+from django.core.exceptions import ObjectDoesNotExist
 from . import models
 
-def download_task_function(request):
 
+def download_task_function(request):
     if request.method == 'GET':
         task_id = request.GET.get('task_id')
-        result = models.TASK.objects.get(TASK_ID=task_id)
-        if result.STATUS == 'UPLOADED':
+        try:
+            result = models.TASK.objects.get(TASK_ID=task_id)
+        except ObjectDoesNotExist:
+            result = None
+
+        if result is not None and result.STATUS == 'UPLOADED':
             video_path = result.FILE_PATH
             print(video_path)
-            video = open(video_path,'rb')
+            video = open(video_path, 'rb')
             return FileResponse(video, content_type='video/mp4')
         else:
             return HttpResponseNotFound()
 
     else:
         return HttpResponseNotAllowed("POST")
-
-
-
-
-
-
